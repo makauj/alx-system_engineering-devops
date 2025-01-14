e5c6b09e69062784eff3cacad469ce80ad72e2fa
Makau <makauwanyoike@gmail.com>
2025-01-14 10:30:03 +0300

    comments

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 #Puppet script to create ssh config file
 
-file { 'ssh_config':
-  ensure  => 'file',
-  path    => '/home/ubuntu/.ssh/config',  # Ensure this is for the correct user
-  owner   => 'ubuntu',                    # Ensure the file is owned by the user (change if needed)
-  group   => 'ubuntu',                    # Group of the user (change if needed)
-  mode    => '0600',                      # Ensure the file is read/write only for the user
-  content => "Host *\n" +
-             "    IdentityFile ~/.ssh/school\n" +
-             "    PasswordAuthentication no\n",
+$file_content = file('/etc/ssh/ssh_config')
+$config = "${file_content}\
+	IdentityFile ~/.ssh/school
+	PasswordAuthentication no
+"
+file {  'school':
+  ensure  => 'present',
+  path    => '/etc/ssh/ssh_config',  # Ensure this is for the correct user
+  content => $config
 }
