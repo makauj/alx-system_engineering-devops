248a2d98fd8822b5e734618e3036a4057badebae
makau <makauwanyoike@gmail.com>
2025-02-17 09:28:47 +0300

    refactor: improve variable naming and URL construction in CSV export script

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -4,18 +4,20 @@ import requests
 import sys
 
 
-if __name__ == "__main__":
-    user_id = sys.argv[1]
-    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
-    response = requests.get(url)
-    user = response.json().get('username')
-    todo_url = "https://jsonplaceholder.typicode.com/users/todos"
-    response = requests.get(todo_url)
-    todos = response.json()
+if __name__ == '__main__':
+    employeeId = sys.argv[1]
+    baseUrl = "https://jsonplaceholder.typicode.com/users"
+    url = baseUrl + "/" + employeeId
 
-    with open(f"{user_id}.csv", 'w') as csvfile:
-        for task in todos:
-            csvfile.write('"{}","{}","{}","{}"\n'.format(
-                user_id, user,
-                task.get("completed"),
-                task.get("title")))
+    response = requests.get(url)
+    username = response.json().get('username')
+
+    todoUrl = url + "/todos"
+    response = requests.get(todoUrl)
+    tasks = response.json()
+
+    with open('{}.csv'.format(employeeId), 'w') as file:
+        for task in tasks:
+            file.write('"{}","{}","{}","{}"\n'
+                       .format(employeeId, username, task.get('completed'),
+                               task.get('title')))
