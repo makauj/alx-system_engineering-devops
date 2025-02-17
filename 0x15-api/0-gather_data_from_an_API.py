a2e4ffa5d0b252e0c6ba457145f94ef51c5829b2
makau <makauwanyoike@gmail.com>
2025-02-17 08:51:49 +0300

    updated code

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,26 +2,34 @@
 """Gather data using api"""
 import requests
 import sys
+import json
 
 
 def get_data(employee_id):
     """Get data from api"""
     url = "https://jsonplaceholder.typicode.com"
-    user = requests.get(url + "/users/{}".format(employee_id)).json()
+    user_res = requests.get(url + "/users/{}".format(employee_id))
+    user = user_res.json()
+    if not user:
+        return (None, None)
+    todos_res = requests.get(url + "/todos", params={"userId": employee_id})
+    todos = todos_res.json()
+    if not todos:
+        return (None, None)
 
-    todos = requests.get(url + "/todos", params={"userId": employee_id}).json()
-    return user, todos
+    return (user, todos)
 
 
 def export_data(employee_id):
-    """Export data to csv"""
+    """Export data to json"""
     user, todos = get_data(employee_id)
-    employee_name = user.get("username")
-    filename = "{}.csv".format(employee_id)
-
-    with open(filename, "w") as f:
-        for todo in todos:
-            f.write('"{}","{}","{}","{}"\n'.format(
-                employee_id, employee_name,
-                todo.get("completed"), todo.get("title")))
-    return filename
+    if not user or not todos:
+        return
+    username = user.get("username")
+    data = []
+    for todo in todos:
+        data.append({"task": todo.get("title"),
+                     "completed": todo.get("completed"),
+                     "username": username})
+    with open("{}.json".format(employee_id), "w") as jsonfile:
+        jsonfile.write(json.dumps({employee_id: data}))
