96f541e3402e89e3d33e8bec0afa51010af9386f
makau <makauwanyoike@gmail.com>
2025-02-18 02:17:29 +0300

    updated code

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -1,26 +1,28 @@
 #!/usr/bin/python3
-"""List of dictionaries"""
+"""Gather data using api"""
 import json
 import requests
 
 
-url = "https://jsonplaceholder.typicode.com"
+url = 'https://jsonplaceholder.typicode.com'
 
-if __name__ == "__main__":
-    user_url = requests.get('{}/users'.format(url)).json()
-    todos_url = requests.get('{}/todos'.format(url)).json()
-    user_data = {}
 
-    for user in user_url:
-        user_id = user.get('id')
+if __name__ == '__main__':
+    users = requests.get('{}/users'.format(url)).json()
+    todo_requests = requests.get('{}/todos'.format(url)).json()
+    users_data = {}
+    for user in users:
+        id = user.get('id')
         user_name = user.get('username')
-        todos = list(filter(lambda x: x.get('userId') == user_id, todos_url))
-        user_data = list(map(lambda x: {
-            "username": user_name,
-            "task": x.get('title'),
-            "completed": x.get('completed')
-        }, todos
+        todos = list(filter(lambda x: x.get('userId') == id, todo_requests))
+        user_data = list(map(
+            lambda x: {
+                'username': user_name,
+                'task': x.get('title'),
+                'completed': x.get('completed')
+            },
+            todos
         ))
-        user_data['{}'.format(user_id)] = user_data
-    with open('todo_all_employees.json', 'w') as f:
-        json.dump(user_data, f)
+        users_data['{}'.format(id)] = user_data
+    with open('todo_all_employees.json', 'w') as file:
+        json.dump(users_data, file)
