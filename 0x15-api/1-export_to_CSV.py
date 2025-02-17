8f30237ca9a5bce6ac410fd47e472336f97a6c42
makau <makauwanyoike@gmail.com>
2025-02-17 09:19:17 +0300

    export data

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     user_id = sys.argv[1]
     url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
     response = requests.get(url)
-    user = response.json()
+    user = response.json().get("username")
     todo_url = "https://jsonplaceholder.typicode.com/users/todos"
     response = requests.get(todo_url)
     todos = response.json()
@@ -18,4 +18,5 @@ if __name__ == "__main__":
         for task in todos:
             csvfile.write('"{}","{}","{}","{}"\n'.format(
                 user_id, user.get("username"),
-                task.get("completed"), task.get("title")))
+                task.get("completed"),
+                task.get("title")))
---
+++ 0x15-api/2-export_to_JSON.py
@@ -0,0 +1,27 @@
+#1/usr/bin/python3
+"""exoprt to json file"""
+import json
+import requests
+
+
+def export_to_json():
+    """export to json file"""
+    url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = 'https://jsonplaceholder.typicode.com/users'
+    user = requests.get(user_url).json()
+    user_dict = {}
+    for i in user:
+        user_dict[i.get('id')] = i.get('username')
+    r = requests.get(url)
+    data = r.json()
+    user_data = {}
+    for i in data:
+        user_id = i.get('userId')
+        if user_id not in user_data:
+            user_data[user_id] = []
+        user_data[user_id].append({"task": i.get('title'),
+                                   "completed": i.get('completed'),
+                                   "username": user_dict.get(user_id)})
+    with open('todo_all_employees.json', 'w') as f:
+        json.dump(user_data, f)
+        
