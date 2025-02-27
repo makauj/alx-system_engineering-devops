4670c1b324a83e45a72a2ceda4f4316438d6d67d
makau <makauwanyoike@gmail.com>
2025-02-27 21:13:44 +0300

    starce

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
-# Puppet script to fix Apache 500 error on a Wordpress website
+# Puppet script to fix Apache 200 error on a Wordpress website
 # fix faulty worpress website
 
-exec { 'fix_apache_500_error':
+exec { 'fix_apache_200_error':
   command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
-  /var/www/html/wp-settings.php; service apache2 restart"',
+/var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
-}
+  }
