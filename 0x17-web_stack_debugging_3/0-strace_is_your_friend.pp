22909642c9941fcca4972ffe862727977d9a73a2
makau <makauwanyoike@gmail.com>
2025-02-27 20:35:00 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,7 +2,7 @@
 # fix faulty worpress website
 
 exec { 'fix_apache_500_error':
-  command => 'bash -c "sed -i s/class-wplocale.phpp/class-wp-locale.php/ \
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/ \
   /var/www/html/wp-settings.php && service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
 }
