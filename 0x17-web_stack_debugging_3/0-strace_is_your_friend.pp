22c127972e6d3abd8fe438fca10996661160ebc8
makau <makauwanyoike@gmail.com>
2025-02-27 21:17:25 +0300

    strace

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
 # Puppet script to fix Apache 200 error on a Wordpress website
 # fix faulty worpress website
 
-$error_file = '/var/www/html/wp-settings.php'
 exec { 'fix_apache_200_error':
-  command => 'sed -i "s/phpp/php/g" $error_file',
+  command => 'bash -c "sed -i s/class-wp-locale.phpp/class-wp-locale.php/g \
+/var/www/html/wp-settings.php; service apache2 restart"',
   path    => '/usr/bin:/usr/sbin:/bin'
   }
